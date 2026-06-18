db164dbacb0bb7ba2102ae3f0610ac2d61dceb83
wesdresd11 <misterbizness777@tutamail.com>
2026-06-18 11:12:55 +0300

    Вторые ручные тесты
    Исправлены ошибки предыдущих тестов, добавил if для проверки подходят значения для решения уравнения или нет. Все 3 теста пройдены успешно

--- equation.py
+++ equation.py
@@ -1,4 +1,8 @@
 
 
 def equation(a: float, b: float) -> float:
+    if a == b:
+        raise ZeroDivisionError("Ошибка деления на ноль, a не должно быть равно b")
+    if a > b:
+        raise ValueError("a не должно быть больше b, так как корень будет считаться из отрицательного чисал, и на выходе получим комплексное")
     return (((a * b) / (a - b)) + (b - a) ** 0.5)
